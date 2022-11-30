5ce2add8ccb278879d950ea3c71481e1b36a78b5
mohammadrezasolo69 <mohammadrezasolo69@gmail.com>
2022-11-30 13:33:18 +0330

    create app users , write CustomUser model and admin

--- core/settings.py
+++ core/settings.py
@@ -22,12 +22,20 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # core
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 3rd party
+    'django_cassandra_engine',
+
+    # my apps
+    'users.apps.UsersConfig',
+
 ]
 
 MIDDLEWARE = [
@@ -119,3 +127,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.CustomUser'
